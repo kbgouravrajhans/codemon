fb705d5e633c8e97338b5631bc40b741cfdecdda
Gourav <kbgaurav004@gmail.com>
2023-03-29 00:44:07 +0530

    bot response checking in console

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.post('/', async (req, res) => {
       bot: response.data.choices[0].text
     });
 
-    console.log(response.data.choices[0].text)
+    console.log(bot)
 
   } catch (error) {
     console.error(error)
